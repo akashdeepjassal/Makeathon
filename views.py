5c70fb960c9914ab5a6ac8785672793c2511c1e0
PritishSehzpaul <pritishsehzpaul123@gmail.com>
2019-03-08 16:31:17 +0530

    Added logging

--- views.py
+++ views.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    app.logger.info("User pinged {} endpoint".format(request.url))
     return render_template('index.html')
 
 
 @app.route('/v1/current_state', methods=['POST'])
 def get_current_state():
+    app.logger.info("User pinged {} endpoint".format(request.url))
     pass
 
 
 @app.route('/v1/test', methods=['GET'])
 def test_url():
+    app.logger.info("User pinged {} endpoint".format(request.url))
     data = request.args.get('test_data')
     if data is None:
         return 'API Parameter incorrect'
